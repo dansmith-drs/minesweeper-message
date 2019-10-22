8ef788b53e82bad6a75d63c9225a134da5b9db55
Dan Smith <dansmith.2011@my.bristol.ac.uk>
2019-10-22 21:19:25 +0100

    Smile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,14 @@ import * as React from 'react'
 import Page from '../components/Page'
 import Container from '../components/Container'
 import { Board } from '../components/minesweeper/Board'
-import { missYou } from '../components/minesweeper/data'
+import { smile } from '../components/minesweeper/data'
 
 import pookey from '../content/images/pookey.jpg'
 
 const Message = () => (
   <Page>
     <Container>
-      <Board height={0} width={0} mines={0} data={missYou} debug={false} />
+      <Board height={0} width={0} mines={0} data={smile} debug={false} />
       <img src={pookey} height={'100px'} style={{ margin: '1rem auto 0 auto', display: 'block' }} alt="Logo" />
     </Container>
   </Page>
